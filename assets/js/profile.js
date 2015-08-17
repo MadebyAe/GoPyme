9981bcdc4fce5daa5ef12b5903ebc60b940b0d1b
Negrete <aa323059@macc02gy23qdv7m.na.bestbuy.com>
2015-08-17 10:36:51 -0500

    get user profile

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -3,6 +3,7 @@ IN.Event.onOnce(IN, 'frameworkLoaded', function initProfile(){
 		console.log('auth');
 		IN.API.Raw("/people/~:(id,first-name,last-name,num-connections,picture-url,picture-urls::(original),summary,skills,positions)").result(function(data){
 			console.log(data);
+			// Aquí se envían los datos al server...
 		}).error(function(error){
 			console.log(error);
 		});
